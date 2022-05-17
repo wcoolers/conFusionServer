840a0666fd98f41f2e96a9d8af039e6887d47142
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2022-05-17 22:19:00 +0100

    https

--- app.js
+++ app.js
@@ -26,6 +26,15 @@ connect.then( (db) => {
 
 const app = express();
 
+//redirect http requests to https
+app.all("*", (req, res, next) => {
+    if (req.secure) {
+        return next()
+    } else {
+        res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url)
+    }
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -53,18 +62,18 @@ app.use('/promotions', promotionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;

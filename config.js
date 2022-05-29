92bfe986ad1f3145a035c1c60dcc36c6971b1e47
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2022-05-29 22:49:41 +0100

    passport-facebook OAuth2

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 //centralize configuration for our server
 module.exports = {
     'secretKey': '12345-7890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1918148718575696',
+        clientSecret: '6180b921d38a9a0e6a8458a83ac63a84'
+    }
 }
